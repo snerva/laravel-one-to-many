f9a64bbd8b87093d9ec6d5d9e6a99abfedcf5188
snerva <selenenerva@live.it>
2023-01-16 18:00:46 +0100

    add relationship on each model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,16 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
 
 
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'slug', 'cover_image', 'description'];
+    protected $fillable = ['title', 'slug', 'cover_image', 'description', 'type_id'];
     public static function generateSlug($title)
     {
         $project_slug = Str::slug($title);
         return $project_slug;
+    }
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
     }
 }
